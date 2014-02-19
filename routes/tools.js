62adca4d146dbced3b5f649c279dcb86b83e188b
John Clark <socrates37@gmail.com>
2014-02-19 15:58:02 -0700

    file conversion to tiff complete

--- routes/tools.js
+++ routes/tools.js
@@ -1,17 +1,31 @@
 module.exports = function(server, settings) {
 
-  var util = require('util');
+  var util = require('util'),
+      exec = require('child_process').exec;
 
   server.post('/v0/tools/convert', function(req, res, next) {
     var widthHeight = '',
         imagePath = req.files.image.path,
         outputPath = req.files.image.path + '.tif';
-    //console.log(req.body);
-    console.log(req.files.image.path);
+    
     var command = util.format('gdal_translate %s %s -co PHOTOMETRIC=RGB -co PROFILE=BASELINE %s',imagePath, widthHeight, outputPath);
-    console.log(command)
+    //console.log(command)
 
-    res.json(true);
+    // Call command using exec
+    exec(command, function(error, stdout, stderr) {
+      // Clean up original image
+      
+      if(error) {
+        // Error Out
+        res.json(error);
+      } else {
+        // Set headers and pipe image to output
+        var stream = fs.createReadStream(outputPath);
+
+        res.setHeader('content-type', 'image/tiff');
+        stream.pipe(res);
+      }
+    });
   });
 
   server.post('/v0/tools/warp', function(req, res, next) {
