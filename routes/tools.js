86c4e890c83d9e252220e96cb5c7c33cca219582
John Clark <socrates37@gmail.com>
2014-02-19 16:42:32 -0700

    put in gdal commands

--- routes/tools.js
+++ routes/tools.js
@@ -14,7 +14,7 @@ module.exports = function(server, settings) {
     // Call command using exec
     exec(command, function(error, stdout, stderr) {
       // Clean up original image
-      
+
       if(error) {
         // Error Out
         res.json(error);
@@ -29,6 +29,19 @@ module.exports = function(server, settings) {
   });
 
   server.post('/v0/tools/warp', function(req, res, next) {
+
+    var imagePath = req.files.image.path,
+        tmpPath = imagePath + '.vrt',
+        outputPath = tmpPath + '.tif',
+        gcpString = '';
+
+    var command = util.format("gdal_translate -a_srs WGS84 -of VRT %s %s %s", imagePath, tmpPath, gcpString);
+
+    var command = util.format("gdalwarp %s %s %s -dstalpha %s -s_srs WGS84 %s %s -co TILED=YES -co COMPRESS=LZW", memoryLimit, transformOption, resampleOption, maskOption, tmpPath, outputPath);
+
+    // We may not need this one?
+    var command = util.format("gdaladdo -r average %s 2 4 8 16 32 64", outputPath);
+
     res.json(true);
   });
 
